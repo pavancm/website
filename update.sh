91fb0d755e818b4e50324f3bd123d326d30f8097
Pavan Chennagiri <pavan@Pavans-MBP.lan>
2024-02-04 17:13:45 -0600

    Initial commit

--- update.sh
+++ update.sh
@@ -4,9 +4,16 @@ git add .
 git commit -m "Initial commit"
 git push -u origin master
 
-hugo
-cd public
+#hugo
+#cd public
+#git add .
+#git commit -m "Build website"
+#git push origin master
+#cd ..
+
+hugo -d ../pavancm.github.io # Deploy your website
+cd ../pavancm.github.io
 git add .
-git commit -m "Build website"
-git push origin master
+git commit -m "first commit"
+git push -u origin master
 cd ..
